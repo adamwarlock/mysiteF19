7930c6ff2e85b03459ce517e48e4b48fb92bf6c7
adam <vivekchopra42@outlook.com>
2019-11-24 18:26:28 -0500

    lab11 - complete

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Publisher, Book, Member, Order, Review
+
+
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    fields = [('title', 'category', 'publisher'), ('num_pages', 'price', 'num_reviews')]
+    list_display = ('title', 'category', 'price')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['books', ('member', 'order_type', 'order_date')]
+    list_display = ('id', 'member', 'order_type', 'order_date', 'total_items')
+
 
 admin.site.register(Publisher)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Member)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Review)
-
-#class PublisherAdmin(admin.ModelAdmin):
